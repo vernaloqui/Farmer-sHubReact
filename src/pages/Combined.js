4a1366b941ea98970efb62753211181c55c11679
vernaloqui <vernadetteloquinario@gmail.com>
2022-12-10 16:37:28 +0800

    banner

--- src/pages/Combined.js
+++ src/pages/Combined.js
@@ -1,8 +1,9 @@
+import banner from "../img/fruitsPage.png"
 function Produce(){
     return(
         <section>
             <div class="container-fluid d-flex m-auto p-0">
-                <img src={"./img/fruitsPage.png"} alt="banner" class="d-block w-100"/>
+                <img src={banner} alt="banner" class="d-block w-100"/>
             </div><br></br>
         
 
