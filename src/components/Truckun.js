73e9e2063e1a8cfd488b0e500bc25f7ad3959382
BellaSia <113756793+BellaSia@users.noreply.github.com>
2023-01-19 19:19:27 +0800

    Add files via upload

---
+++ src/components/Truckun.js
@@ -0,0 +1,21 @@
+import "./truckun.css";
+function Truckun (){
+    return(
+        <div className="wrapper">
+          <div className="row g-2 d-flex justify-content-center">
+
+<div className="loop-wrapper">
+  <div className="mountain"></div>
+  <div className="hill"></div>
+  <div className="tree"></div>
+  <div className="tree"></div>
+  <div className="tree"></div>
+  <div className="rock"></div>
+  <div className="truck"></div>
+  <div className="wheels"></div>
+</div> 
+</div>
+        </div>
+    )
+}
+export default Truckun;
